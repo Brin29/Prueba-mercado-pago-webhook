197b81443b54c4cd073a15755dd9fe604a128798
Brin29 <breinerstivenparracortes50@gmail.com>
2024-10-21 10:31:21 -0500

    first commit

---
+++ webhook/src/main/java/prueba/webhook/WebhookApplication.java
@@ -0,0 +1,13 @@
+package prueba.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookApplication.class, args);
+	}
+
+}
---
+++ webhook/src/test/java/prueba/webhook/WebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
